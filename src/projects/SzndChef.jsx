461b2bb492c318f820a7caea32f960b39a2a8482
SmithNJ13 <smithNJ13@hotmail.com>
2025-02-08 09:57:24 +0000

    new images added, SzndChef project formatted, with more description

--- src/projects/SzndChef.jsx
+++ src/projects/SzndChef.jsx
@@ -6,6 +6,9 @@ import { G1, D1, O1} from '../yapfile.js'
 import Main from '../components/Main/index.jsx'
 import { imgBorder } from '../slayfile.js'
 import ScrollTo from '../components/ScrollToTop/index.jsx'
+import LoadingImage from "../assets/ProjectOne/LoadingNav.png"
+import SeasonalImage from "../assets/ProjectOne/SeasonalPage.png"
+import UpdatedLoading from "../assets/ProjectOne/UpdatedLoading.png"
 
 const SzndChef = () => {
   return (
@@ -22,12 +25,27 @@ const SzndChef = () => {
           <section className="flex flex-col m-[1rem]">
             <h2 className="font-bold underline text-2xl">Development:</h2>
             <p className="mx-[1rem]">{D1}</p>
-            <img src={img1} className={imgBorder}></img>
+            <p className="mx-[1rem] mt-[1rem]">When tackling this, I knew going in that I wanted it to all be on a single page, I started by creating an outline on the
+              left side of the screen - where the NavBar would eventually be placed. I gave this a background colour to test different states, I wanted the background and
+              theme of the page to change with each season. In this case I made summer yellow, and when switching to spring, autumn or winter the theme would change to green,
+              orange or blue. I also implemented a carousel that featured placeholder images of fruit to test functionality.
+            </p>
+            <img src={SeasonalImage} className={`${imgBorder} sm:w-[60vw] sm:h-[70vh] mt-[1rem]`}></img>
+            <i className="text-sm text-center">*As this image has been taken retroactively, the images used are not stored and thus only display as "..."</i>
+            <p className="mx-[1rem] mt-[1rem]">I also implemented a temporary 'loading' page so there wouldn't be problems with unloaded assets. As well as a rudimentary
+              NavBar for the sake of visualisation and testing endpoints.
+            </p>
+            <img src={LoadingImage} className={`${imgBorder} sm:w-[60vw] sm:h-[70vh] my-[1rem]`}></img>
+            <p className="mx-[1rem] mt-[1rem]">Further down the development line the loading page eventually looked like the below image, in addition to this you can see the background changing colour
+              as the season currently being navigated to is winter.
+            </p>
+            <img src={UpdatedLoading} className={`${imgBorder} mt-[1rem]`}></img>
           </section>
           <section className="flex flex-col m-[1rem]">
             <h2 className="font-bold underline text-2xl">Outcome:</h2>
             <p className="mx-[1rem]">{O1}</p>
-            <img src={img2} className={imgBorder}></img>
+            <img src={img1} className={`${imgBorder} my-[1rem]`}></img>
+            <img src={img2} className={`${imgBorder} my-[1rem]`}></img>
           </section>
           <section className="flex flex-col m-[1rem]">
             <h2 className="font-bold underline text-2xl">Tools & Repo:</h2>
--- src/yapfile.js
+++ src/yapfile.js
@@ -8,8 +8,13 @@
 const G1 = `The objective of this project was to build a full-stack web application that is deployed and accessible by anyone. The users that sign up will be able to login and
               browse through a variety of recipes, both default and user-created. They would also be able to comment their thoughts on each recipe, as well as favourite ones that they
               like. The core theme and selling point of this web application is the "seasonal" focus, being able to filter recipes by their ideal season.`
-const D1 = `Development of some kind`
-const O1 = `Outcome of some kind`
+const D1 = `As a group project, development initially started by us making a trello board and getting in a call to discuss what we want from the application and which tasks each team member
+is going to be assigned to, based on strengths and preferences. At the time, I was learning to properly use react states and given this was a seasonally themed recipe app, I took on the task
+of designing the 'seasonal page' with it in mind that each season can be a state.`
+const O1 = `The outcome of this project was a successfully deployed and functional full-stack seasonal recipe web application. We hit most of our target goals, by regularly checking the
+trello board and working on our assigned tasks for that day. The seasonal page that I was designated to work on met my standards and was able to change themes dynamically, remain as a single page,
+display all associated seasonal recipes as well as include a filter function by price and dietry restrictions, the user is also able to navigate to each displayed recipe for
+information on how to cook it, as well as save the recipe to their 'likes' and comment their thoughts on it.`
 
 /* Project: Premier League Predictions, full-stack prediction web application
     relevant string information is found below */
