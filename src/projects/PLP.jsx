e65a6939b2451e4b549138f381067aef90ce0b37
SmithNJ13 <smithNJ13@hotmail.com>
2025-02-08 08:16:56 +0000

    done formating + descriptions for implementation 1 & 2 on PLP

--- src/projects/PLP.jsx
+++ src/projects/PLP.jsx
@@ -74,19 +74,19 @@ const PLP = () => {
             <><div className="sm:mx-[2rem] sm:my-[2rem] m-[1rem]">
               <p>{D2} <br />Here is an image of what the first implementation of the cards looked like:</p>
             </div>
-            <div className="flex sm:flex-row sm:mx-[4rem] sm:gap-[2rem] flex-wrap">
+            <div className="flex sm:flex-row sm:mx-[4rem] sm:gap-[2rem] sm:flex-nowrap flex-wrap">
               <img src={card} className={`${imgBorder}`}></img>
               <p className="m-[1rem]">While setting up a very basic design and layout for the cards, I also mapped basic routing for both a 'profile' page and a 'login' page.
-                <br />I then spent more time on further developing the card, adding the following features:
+              <br />I then spent more time on further developing the card, adding the following features:
                 <div className="mx-[1rem]">
                   <li>Card colours match that of their respective team icon</li>
-                  <li>Cards became a part of a 'teamBanner' component</li>
+                  <li className="mb-[1rem]">Cards became a part of a 'teamBanner' component</li>
                 </div>
               <img src={img2} className={imgBorder}></img>
               </p>
             </div>
             <div className="flex flex-col m-[1rem]">
-              <p className="self-center">Finally, I decided to add a NavBar and do some minor styling to the overall webpage, I was trying to express some creativity with an 'edgy' design</p>
+              <p className="self-center mb-[1rem]">Finally, I decided to add a NavBar and do some minor styling to the overall webpage, I was trying to express some creativity with an 'edgy' design</p>
               <img src={img3} className={imgBorder}></img>
               <i className="text-sm text-center">*I retroactively took this screenshot so the premier league team that usually renders in is not included in the current dataset</i>
             </div>
@@ -96,20 +96,37 @@ const PLP = () => {
             <>
             <h2 className="m-[1rem] text-2xl font-bold">Frontend:</h2>
             <div className="flex flex-col items-center">
-              <p className="sm:mx-[2rem] mx-[1rem] mb-[1rem]">Here I overhauled the layout and colour scheme of the website as well as allowed the live matches to display games beyond just Arsenal matches.</p>
+              <p className="sm:mx-[2rem] mx-[1rem] mb-[1rem]">I wanted the website layout to look a bit cleaner and have more visually appealing colours, since the current focus of the website was on Arsenal premier league matches
+                I decided to make the colour scheme similar to colours found on the Arsenal logo.
+              </p>
               <img src={img4} className={imgBorder}></img>
-              <p className="sm:mx-[7rem] my-[1rem] mx-[1rem]">Additionally, I added a 'profile' page which displays a prediction table showcasing a history of the users most recent predictions, as well a
+              <p className="sm:mx-[7rem] my-[1rem] mx-[1rem]">After some development, I decided that the focus of just Arsenal only matches was too narrow a focus and I wanted to expanded the
+                project to cover more - so I opened it up to every premier league match.
+                <br className="mb-[1rem]"/>Additionally, I added a 'profile' page which displays a prediction table showcasing a history of the users most recent predictions, as well a
                 'net xG' rating, which is a metric one can use to assess how accurate their predictions are.
               </p>
               <img src={img5} className={imgBorder}></img>
             </div>
             <h2 className="m-[1rem] text-2xl font-bold">Backend:</h2>
-            <div className="flex sm:flex-row gap-[1rem] justify-around m-[1rem] flex-col">
-              <img src={be1} className={imgBorder}></img>
-              <img src={be2} className={imgBorder}></img>
-            </div>
-            <p className="text-center sm:mx-[6rem] mx-[1rem]">I also set up the backend data structures and endpoints, so that the matches could be displayed with their relevant data as well as setting
-              up the prediction class for when the user makes a prediction.
+            <p className="my-[1rem] mx-[2rem]">On the backend while the focus was just on Arsenal matches, I had set up the basic data structure for the Match class, which included a:
+              <li className="ml-[1rem]">Unique ID</li>
+              <li className="ml-[1rem]">Match date</li>
+              <li className="ml-[1rem]">Opponent (who Arsenal were against)</li>
+              <li className="ml-[1rem]">An expected goal value (xG)</li>
+              <li className="ml-[1rem]">An expected goal value for the opposing team (xGA)</li>
+            </p>
+            <img src={be1} className={`${imgBorder} sm:w-[43rem] sm:h-[19rem]`}></img>
+            <p className="my-[1rem] mx-[2rem]">In addition to this, I had also set up the data structure for making a Prediction, this included the following variables:
+            <li className="ml-[1rem]">Unique ID</li>
+            <li className="ml-[1rem]">The user ID</li>
+            <li className="ml-[1rem]">The match ID</li>
+            <li className="ml-[1rem]">The predicted expected goal value (xG)</li>
+            <li className="ml-[1rem]">The predicted expected goal value against (xGA) i.e. the opponents xG</li>
+            </p>
+            <img src={be2} className={imgBorder}></img>
+            <p className="text-center sm:mx-[6rem] mx-[1rem] my-[1rem]">The result of this implementation allowed the user to make a prediction on any Arsenal match for both
+              Arsenal and their opponent - and the relevant information would be stored to the backend database (UserID, matchID, predicted_xG and predicted_xGA) each prediction
+              would have it's own unique ID for easy identification, this would also allow me to fetch all of the predictions for a specific user, or for a specific match if needed.
             </p>
             </>
           )}
@@ -117,20 +134,37 @@ const PLP = () => {
             <>
             <h2 className="m-[1rem] text-2xl font-bold">Frontend:</h2>
             <div className="flex flex-col items-center">
-              <p className="sm:mx-[2rem] mx-[1rem] mb-[1rem]">Here I overhauled the layout and colour scheme of the website as well as allowed the live matches to display games beyond just Arsenal matches.</p>
+              <p className="sm:mx-[2rem] mx-[1rem] mb-[1rem]">I wanted the website layout to look a bit cleaner and have more visually appealing colours, since the current focus of the website was on Arsenal premier league matches
+                I decided to make the colour scheme similar to colours found on the Arsenal logo.
+              </p>
               <img src={img6} className={imgBorder}></img>
-              <p className="sm:mx-[7rem] my-[1rem] mx-[1rem]">Additionally, I added a 'profile' page which displays a prediction table showcasing a history of the users most recent predictions, as well a
+              <p className="sm:mx-[7rem] my-[1rem] mx-[1rem]">After some development, I decided that the focus of just Arsenal only matches was too narrow a focus and I wanted to expanded the
+                project to cover more - so I opened it up to every premier league match.
+                <br className="mb-[1rem]"/>Additionally, I added a 'profile' page which displays a prediction table showcasing a history of the users most recent predictions, as well a
                 'net xG' rating, which is a metric one can use to assess how accurate their predictions are.
               </p>
               <img src={img7} className={imgBorder}></img>
             </div>
             <h2 className="m-[1rem] text-2xl font-bold">Backend:</h2>
-            <div className="flex sm:flex-row gap-[1rem] justify-around m-[1rem] flex-col">
-              <img src={be3} className={imgBorder}></img>
-              <img src={be4} className={imgBorder}></img>
-            </div>
-            <p className="text-center sm:mx-[6rem] mx-[1rem]">I also set up the backend data structures and endpoints, so that the matches could be displayed with their relevant data as well as setting
-              up the prediction class for when the user makes a prediction.
+            <p className="my-[1rem] mx-[2rem]">On the backend while the focus was just on Arsenal matches, I had set up the basic data structure for the Match class, which included a:
+              <li className="ml-[1rem]">Unique ID</li>
+              <li className="ml-[1rem]">Match date</li>
+              <li className="ml-[1rem]">Opponent (who Arsenal were against)</li>
+              <li className="ml-[1rem]">An expected goal value (xG)</li>
+              <li className="ml-[1rem]">An expected goal value for the opposing team (xGA)</li>
+            </p>
+            <img src={be3} className={`${imgBorder} sm:w-[43rem] sm:h-[19rem]`}></img>
+            <p className="my-[1rem] mx-[2rem]">In addition to this, I had also set up the data structure for making a Prediction, this included the following variables:
+            <li className="ml-[1rem]">Unique ID</li>
+            <li className="ml-[1rem]">The user ID</li>
+            <li className="ml-[1rem]">The match ID</li>
+            <li className="ml-[1rem]">The predicted expected goal value (xG)</li>
+            <li className="ml-[1rem]">The predicted expected goal value against (xGA) i.e. the opponents xG</li>
+            </p>
+            <img src={be4} className={imgBorder}></img>
+            <p className="text-center sm:mx-[6rem] mx-[1rem] my-[1rem]">The result of this implementation allowed the user to make a prediction on any Arsenal match for both
+              Arsenal and their opponent - and the relevant information would be stored to the backend database (UserID, matchID, predicted_xG and predicted_xGA) each prediction
+              would have it's own unique ID for easy identification, this would also allow me to fetch all of the predictions for a specific user, or for a specific match if needed.
             </p>
             </>
           )}
