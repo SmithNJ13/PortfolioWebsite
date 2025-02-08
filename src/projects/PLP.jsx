db9f07ab39f693dd4ec0f2c553805575e30a57ab
SmithNJ13 <smithNJ13@hotmail.com>
2025-02-08 08:35:44 +0000

    formatting and description for implementation #3

--- src/projects/PLP.jsx
+++ src/projects/PLP.jsx
@@ -134,37 +134,41 @@ const PLP = () => {
             <>
             <h2 className="m-[1rem] text-2xl font-bold">Frontend:</h2>
             <div className="flex flex-col items-center">
-              <p className="sm:mx-[2rem] mx-[1rem] mb-[1rem]">I wanted the website layout to look a bit cleaner and have more visually appealing colours, since the current focus of the website was on Arsenal premier league matches
-                I decided to make the colour scheme similar to colours found on the Arsenal logo.
+              <p className="sm:mx-[2rem] mx-[1rem] mb-[1rem]">In this implementation of the web application, I felt the need to re-evaluate the webpage layout and overall feel of the website. Given that
+                it has now expanded beyond just Arsenal matches to encompass all premier league matches, it felt fitting to remove the Arsenal colours. As I'm really enjoy the usage of the colour green I knew
+                that I wanted to encorporate it into my website and so I tried my own hand at picking a colour palette, I tried to keep the greens a soft, pastel colour so it could be contrasted by darker
+                fonts for readability. I also designed a temporary logo to display on the NavBar for the sake of visualisation, as well as a mock 'news feed' on the far right of the screen.
               </p>
               <img src={img6} className={imgBorder}></img>
-              <p className="sm:mx-[7rem] my-[1rem] mx-[1rem]">After some development, I decided that the focus of just Arsenal only matches was too narrow a focus and I wanted to expanded the
-                project to cover more - so I opened it up to every premier league match.
-                <br className="mb-[1rem]"/>Additionally, I added a 'profile' page which displays a prediction table showcasing a history of the users most recent predictions, as well a
-                'net xG' rating, which is a metric one can use to assess how accurate their predictions are.
+              <p className="sm:mx-[7rem] my-[1rem] mx-[1rem]">As I had expanded what the user was able to predict on, this had to be reflected on the cards and so the previous 'xG' button
+                was replaced with a simple 'place predictions' button, which upon clicking would play a card flipping animation and reveal a set of predictions the user could make
               </p>
               <img src={img7} className={imgBorder}></img>
             </div>
             <h2 className="m-[1rem] text-2xl font-bold">Backend:</h2>
-            <p className="my-[1rem] mx-[2rem]">On the backend while the focus was just on Arsenal matches, I had set up the basic data structure for the Match class, which included a:
-              <li className="ml-[1rem]">Unique ID</li>
-              <li className="ml-[1rem]">Match date</li>
-              <li className="ml-[1rem]">Opponent (who Arsenal were against)</li>
-              <li className="ml-[1rem]">An expected goal value (xG)</li>
-              <li className="ml-[1rem]">An expected goal value for the opposing team (xGA)</li>
+            <p className="my-[1rem] mx-[2rem]">Having expanded what the user can make predictions on I naturally had to refactor the Prediction class on the backend to make sure the data structure was
+              suited for the new functionality, the new things I added into the data structure were:
+              <li className="ml-[1rem]">side</li>
+              <li className="ml-[1rem]">corners</li>
+              <li className="ml-[1rem]">playerToScore</li>
+              <li className="ml-[1rem]">cleanSheet</li>
+            </p>
+            <p className="my-[1rem] mx-[2rem]">I had also removed predicted_xGA as this was no longer needed, having the 'side' variable meant I could just take the xG of either
+              home or away.
             </p>
             <img src={be3} className={`${imgBorder} sm:w-[43rem] sm:h-[19rem]`}></img>
-            <p className="my-[1rem] mx-[2rem]">In addition to this, I had also set up the data structure for making a Prediction, this included the following variables:
-            <li className="ml-[1rem]">Unique ID</li>
-            <li className="ml-[1rem]">The user ID</li>
-            <li className="ml-[1rem]">The match ID</li>
-            <li className="ml-[1rem]">The predicted expected goal value (xG)</li>
-            <li className="ml-[1rem]">The predicted expected goal value against (xGA) i.e. the opponents xG</li>
+            <p className="my-[1rem] mx-[2rem]">To better manage the predictions I also had to update the Match class file to help accomodate the newly added 'side' variable. <br/>The new things added
+              to the Match class include:
+            <li className="ml-[1rem]">time</li>
+            <li className="ml-[1rem]">home</li>
+            <li className="ml-[1rem]">home_xG</li>
+            <li className="ml-[1rem]">away</li>
+            <li className="ml-[1rem]">away_xG</li>
             </p>
             <img src={be4} className={imgBorder}></img>
-            <p className="text-center sm:mx-[6rem] mx-[1rem] my-[1rem]">The result of this implementation allowed the user to make a prediction on any Arsenal match for both
-              Arsenal and their opponent - and the relevant information would be stored to the backend database (UserID, matchID, predicted_xG and predicted_xGA) each prediction
-              would have it's own unique ID for easy identification, this would also allow me to fetch all of the predictions for a specific user, or for a specific match if needed.
+            <p className="text-center sm:mx-[6rem] mx-[1rem] my-[1rem]">This allowed a new logical flow to making predictions, which would take the 'side' from a match, either
+              home or away and use this data when making a prediction, so if the user made a prediction on a specific card it would get something along the lines of 'home' + 'xG'
+              and then make the predicition of 'home, 3.1' for example.
             </p>
             </>
           )}
@@ -185,21 +189,22 @@ const PLP = () => {
           <h2 className="font-bold underline text-2xl">Outcome:</h2>
           <p>{O2}</p>
         </section>
-        <section className="flex flex-col m-[1rem]">
+        <section className="flex flex-col m-[1rem] ml-[1rem]">
           <h2 className="font-bold underline text-2xl">Tools & Repo:</h2>
-          <li className="ml-[1rem]">React</li>
-          <li className="ml-[1rem]">JavaScript</li>
-          <li className="ml-[1rem]">Axios</li>
-          <li className="ml-[1rem]">React-router</li>
-          <li className="ml-[1rem]">Framer-motion</li>
-          <li className="ml-[1rem]">Git</li>
-          <li className="ml-[1rem]">Tailwindcss</li>
-          <li className="ml-[1rem]">Vite</li>
-          <li className="ml-[1rem]">Cheerio</li>
-          <li className="ml-[1rem]">Mongodb</li>
-          <li className="ml-[1rem]">Cors</li>
-          <li className="ml-[1rem]">Express</li>
-          <li className="ml-[1rem]">bcrypt</li>
+          <li className="ml-[1rem] text-xl">React</li>
+          <li className="ml-[1rem] text-xl">JavaScript</li>
+          <li className="ml-[1rem] text-xl">Axios</li>
+          <li className="ml-[1rem] text-xl">React-router</li>
+          <li className="ml-[1rem] text-xl">Framer-motion</li>
+          <li className="ml-[1rem] text-xl">git</li>
+          <li className="ml-[1rem] text-xl">Tailwindcss</li>
+          <li className="ml-[1rem] text-xl">Vite</li>
+          <li className="ml-[1rem] text-xl">Cheerio</li>
+          <li className="ml-[1rem] text-xl">Mongodb</li>
+          <li className="ml-[1rem] text-xl">Cors</li>
+          <li className="ml-[1rem] text-xl">Express</li>
+          <li className="ml-[1rem] text-xl">bcrypt</li>
+          <li className="ml-[1rem] text-xl">Inkscape</li>
           <Link to="https://github.com/SmithNJ13/PredictionSite" className="font-bold text-center">GitHub repository</Link>
         </section>
         <ScrollTo />
